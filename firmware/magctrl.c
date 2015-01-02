0664fefaf2fe908f7907c472a99374e7f795c679
Wu Hao <whowechina@gmail.com>
2015-01-02 22:10:15 +0800

    1. Boot capacitor to 1uF. 2. Firmware project switched to Atmel Studio 3. RC calibration byte read from EEPROM 4. Frequency to 4.8MHz, Phase Correct PWM.

--- firmware/magctrl.c
+++ firmware/magctrl.c
@@ -1,18 +1,36 @@
-//ICC-AVR application builder : 2013/9/5 21:58:14
-// Target : T13
-// Crystal: 9.6000Mhz
+/*
+ * magctrl.c
+ *
+ * Created: 2014/9/21 21:17:06
+ *  Author: whowe
+ */ 
 
-#include <iot13v.h>
-#include <macros.h>
+#define F_CPU 4800000
 
-#define GREEN PB3
+#include <avr/signature.h>
+const char fusedata[] __attribute__ ((section (".fuse"))) =
+{0x75, 0xFF};
+const char lockbits[] __attribute__ ((section (".lockbits"))) =
+{0xFC};
 
-#define POWER_ZERO 255
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/eeprom.h>
+#include <avr/wdt.h>
+#include <util/delay.h>
 
-#define POWER_FULL 15
-#define POWER_HOLD 170
+#define GREEN PORTB3
 
-#define DEADLOOP -1
+#define RC_CAL_ADDR 0
+#define RC_MAGIC_ADDR 1
+
+#define RC_MAGIC 88
+
+#define POWER_FULL 10
+#define POWER_HOLD 50
+#define POWER_MIN 255
+
+#define INFINITE -1
 
 #define LED_BLINK 1      /* LED blink */
 #define LED_ON 2         /* LED not blink */
@@ -23,9 +41,64 @@
 
 typedef unsigned char byte;
 
+void init_rc();
+void init_port();
+void init_adc();
+void init_pwm();
+
 void led_on(byte led);
 void led_off(byte led);
-void output(int pow);
+void output_pwm(int pow);
+
+/* Output constant power level
+    \param power power (PWM) value 
+    \param duration duration in ms, or INFINITE
+    \param blink LED_BLINK, LED_ON or LED_OFF
+    \param lv_detect DETECT_LV or NO_DETECT_LV
+    \retval 0 OK, -1 Voltage Drop
+*/
+int const_power(byte power, int duration, byte blink, byte lv_detect);
+
+/* Transit power level from begin to end
+    \param begin begin value
+    \param end end value
+    \param wait delay time for each transit step
+*/
+void transit_power(byte begin, byte end, int wait);
+
+void init_rc(void)
+{
+    unsigned char magic;
+    unsigned char cal;
+    
+    magic = eeprom_read_byte((unsigned char *)RC_MAGIC_ADDR);
+    cal = eeprom_read_byte((unsigned char *)RC_CAL_ADDR);
+    
+    if (magic == RC_MAGIC)
+        OSCCAL = cal;
+}
+
+void init_port(void)
+{
+    PORTB = 0x00;    /* Clear Output */
+    DDRB = 1 << PORTB0 | 1 << GREEN; /* PWM and LED */
+}
+
+void init_pwm(void)
+{
+    OCR0A = POWER_MIN; /* Minimal power when init */
+    TCCR0A = 2 << COM0A0 | 1 << WGM00 ;  /* Phase Correct PWM */
+    TCCR0B = 1 << CS00;  /* No Prescaler */
+}
+
+void init_adc(void)
+{
+    ADCSRA |= (1 << ADEN) | (1 << ADATE) | (1 << ADPS1) | (1 << ADPS2);
+    ADCSRB = 0x00;  /* Free running */
+    ADMUX = (0 << REFS0) | (1 << ADLAR) | (1 << MUX0); /* PB2 */
+    DIDR0 = (1 << ADC2D);
+    ADCSRA |= (1 << ADSC);  
+}
 
 void led_on(byte led)
 {
@@ -37,92 +110,9 @@ void led_off(byte led)
     PORTB &= ~(1 << led);
 }
 
-void output(int pow)
+void output_pwm(int pow)
 {
     OCR0A = pow;
-}
-
-void delay(int ms)
-{
-     int i, j;
-     for (i = 0; i < ms; i ++)
-  	    for (j = 0; j < 573; j ++)
-	  	  /* NOP */;
-}
-
-void init_port(void)
-{
-    PORTB = 0x00;
-    DDRB = 0x00;
-    DDRB |= (1 << PB0);
-    DDRB |= (1 << GREEN);
-}
-
-void init_devices(void)
-{
-    CLI();
-    MCUCR = 0x00;
-    TIMSK0 = 0x00;
-    GIMSK = 0x00;
-
-    output(POWER_ZERO);
-
-    TCCR0B = 0;
-    TCCR0A = (0 << WGM01) | (1 << WGM00) ;  /* Phase Correct PWM */
-    TCCR0B |= (0 << WGM02) | (0 << CS01) | (1 << CS00);  /* No Prescaling */
-    TCCR0A |= ((1 << COM0A1) | (0 << COM0A0));
- 
-    SEI();
-}
-
-void init_adc(void)
-{
-    ADCSRA |= (1 << ADEN) | (1 << ADATE) | (1 << ADPS1) | (1 << ADPS2);
-    ADCSRB = 0x00;  /* Free running */
-    ADMUX = (0 << REFS0) | (1 << ADLAR) | (0 << MUX1) | (1 << MUX0); /* PB2 */
-    DIDR0 = (1 << ADC2D);
-    ADCSRA |= (1 << ADSC);  
-}
-
-
-/* Fading test */
-void fading(void)
-{
-    int i;
-    while (1)
-    {
-        for (i = 255; i > 5; i --)
-        {
-            led_on(GREEN);
-            output(i);
-            delay(30);
-        }
-        for (i = 5; i < 255; i ++)
-        {
-            led_off(GREEN);
-            output(i);
-            delay(30);
-        }
-    }
-}
-
-/* ADC test */
-void adctest(void)
-{
-    byte adc, old;
-  
-    while (1)
-    {
-        adc = ADCH;
-        output(adc);
-        if (old != adc)
-        {
-            led_on(GREEN);
-        }
-        old = adc;
-        delay(25);
-        led_off(GREEN);
-    }
 }
 
 /* Output constant power level
@@ -134,7 +124,7 @@ int const_power(byte power, int duration, byte blink, byte lv_detect)
     int t, i;
     byte c, lv;
     
-    output(power);
+    output_pwm(power);
     
     if (blink == LED_ON)
         led_on(GREEN);
@@ -142,7 +132,7 @@ int const_power(byte power, int duration, byte blink, byte lv_detect)
         led_off(GREEN);
         
     c = 0;
-	for (t = 0; (t < duration) || (duration == DEADLOOP); t += 10)
+	for (t = 0; (t < duration) || (duration == INFINITE); t += 10)
 	{
         /* BLINK control */
         if (blink == LED_BLINK)
@@ -164,14 +154,14 @@ int const_power(byte power, int duration, byte blink, byte lv_detect)
                 if (ADCH > 0x08)  /* Voltage gate: 0xff: 5v,  0x10: 0.31v */
                     lv = 0;
                 
-                delay(1);
+                _delay_ms(1);
             }
             if (lv == 1)
                 return -1;
         }
         else
         {
-            delay(10);
+            _delay_ms(10);
         }
     }
     return 0;
@@ -180,7 +170,7 @@ int const_power(byte power, int duration, byte blink, byte lv_detect)
 /* Transit from begin level to end level */
 void transit_power(byte begin, byte end, int wait)
 {
-    int i, step;
+    int i, j, step;
     
     if (end > begin)
         step = 1;
@@ -189,22 +179,26 @@ void transit_power(byte begin, byte end, int wait)
         
     for (i = begin; i != end; i += step)
     {
-        output(i);
-        delay(wait);
+        output_pwm(i);
+        
+        for (j = 0; j < wait; j ++)
+            _delay_ms(1);
     }
 }
 
 /* Main routine */
-void main(void)
+int main(void)
 {
     /* Initialize everything */
-
+    
+    init_rc();
     init_port();   
-    init_devices(); 
-    init_adc();
+    init_pwm(); 
+    
+    //init_adc();
 
-    /* fading(); */
-    /* adctest(); */
+    // fading();
+    // adctest();
     
     /* Phase 1: use full power level */
     const_power(POWER_FULL, 2000, LED_ON, NO_DETECT_LV);
@@ -213,5 +207,50 @@ void main(void)
     transit_power(POWER_FULL, POWER_HOLD, 1);
     
     /* Phase 3: Hold on holding level */
-    const_power(POWER_HOLD, DEADLOOP, LED_BLINK, NO_DETECT_LV);
+    const_power(POWER_HOLD, INFINITE, LED_BLINK, NO_DETECT_LV);
+    
+    return 0;
+}
+
+
+
+/* Fading test */
+void fading(void)
+{
+    int i;
+    
+    while (1)
+    {
+        for (i = 255; i > 0; i --)
+        {
+            led_on(GREEN);
+            output_pwm(i);
+            _delay_ms(30);
+        }
+        for (i = 0; i < 255; i ++)
+        {
+            led_off(GREEN);
+            output_pwm(i);
+            _delay_ms(30);
+        }
+    }
+}
+
+/* ADC test */
+void adctest(void)
+{
+    byte adc, old = 0;
+    
+    while (1)
+    {
+        adc = ADCH;
+        output_pwm(adc);
+        if (old != adc)
+        {
+            led_on(GREEN);
+        }
+        old = adc;
+        _delay_ms(25);
+        led_off(GREEN);
+    }
 }
